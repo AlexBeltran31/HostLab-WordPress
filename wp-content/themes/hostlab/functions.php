661b10959f2b3d693dc96834943665aca010927e
AlexBeltran31 <alexbeltranrt@gmail.com>
2026-09-01 16:56:50 +0200

    Enqueue theme stylesheet and add dependent region-comuna dropdown script

--- wp-content/themes/hostlab/functions.php
+++ wp-content/themes/hostlab/functions.php
@@ -9,3 +9,21 @@ add_action( 'init', function () {
 		array( 'label' => __( 'HostLab', 'hostlab' ) )
 	);
 } );
+
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style(
+		'hostlab-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+
+	$script_path = get_theme_file_path( '/js/regiones-comunas.js' );
+	wp_enqueue_script(
+		'hostlab-regiones-comunas',
+		get_template_directory_uri() . '/js/regiones-comunas.js',
+		array(),
+		file_exists( $script_path ) ? filemtime( $script_path ) : '1.0',
+		true
+	);
+} );
